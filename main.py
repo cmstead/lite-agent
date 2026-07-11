beea45142165bb4fded1140066063a4f20dca056
Chris Stead <cmstead@gmail.com>
2026-07-11 15:57:35 -0700

    Switched back to mini model for cost reasons

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ dotenv.load_dotenv()
 agent = Agent({
         # "model": "ollama/qwen3.5:9b",
         # "api_base": "http://192.168.1.212:11434"
-        "model": "gpt-4o",
+        "model": "gpt-4o-mini",
     }, 
     tools,
     "You are a helpful personal technical assistant. You will be given a task to complete. You will be provided with tools to help you complete the task. You should use the tools to complete the task. You should not make up information. If you are unsure about something, you should ask for clarification.",
